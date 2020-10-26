33f911c1e0699b0f21ccf0bdbc2e084df5afb774
tyrone <songpu_work@163.com>
2020-10-26 11:47:48 +0800

    2020-10-26 init

--- Sources/SPUIColor/SPUIColor.swift
+++ Sources/SPUIColor/SPUIColor.swift
@@ -1,3 +1,43 @@
-struct SPUIColor {
-    var text = "Hello, World!"
+import UIKit
+
+extension UIColor {
+    
+    convenience init(_ hexString: String) {
+        let hexString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red     = CGFloat(r) / 255.0
+        let green   = CGFloat(g) / 255.0
+        let blue    = CGFloat(b) / 255.0
+        
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    
+    convenience init(r : CGFloat, g : CGFloat, b : CGFloat, alpha: CGFloat = 1.0) {
+        self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: alpha)
+    }
+    
+    /// 黑色透明度
+    convenience init(alpha: CGFloat) {
+        self.init(red: 0.0, green: 0.0, blue: 0.0, alpha: alpha)
+    }
+
+    
+    /// 随机色
+    class func random() -> UIColor {
+        return UIColor(red: CGFloat(arc4random()%255)/255, green: CGFloat(arc4random()%255)/255, blue: CGFloat(arc4random()%255)/255, alpha: 1)
+    }
 }
